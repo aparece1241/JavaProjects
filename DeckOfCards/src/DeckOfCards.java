87b925bdf96c2e6e7e6f73cdd36ce80c08a9e62a
caaiyen <ailyn.albores2000@gmail.com>
2020-09-09 08:54:52 +0800

    adds another project

--- DeckOfCards/src/DeckOfCards.java
+++ DeckOfCards/src/DeckOfCards.java
@@ -3,7 +3,8 @@ import java.util.Collections;
 import java.util.Scanner;
 public class DeckOfCards {
 
-    protected static ArrayList<Card> cards;
+    Scanner input = new Scanner(System.in);
+    protected static ArrayList<Card> cards = new ArrayList<Card>();
     /**
      * This function will create cards
      *
@@ -11,9 +12,12 @@ public class DeckOfCards {
      */
     protected void createCards(){
         cards = genCards();
-        System.out.println("Cards are already created!");
+        System.out.println("\n\nCards are already created!\n\n");
     }
 
+    public int getInput(Scanner in){
+        return in.nextInt();
+    }
     /**
      * "This a temporary function"
      * This function will
@@ -26,7 +30,9 @@ public class DeckOfCards {
         String ranks [] = {"2","3","4","5","6",
                           "7","8","9","10","A",
                           "J","Q","K"};
+
         String suits [] = {"H","D","S","C"};
+
         ArrayList<Card> cards = new ArrayList<Card>();
 
         for ( String suit : suits) {
@@ -49,10 +55,11 @@ public class DeckOfCards {
      */
 
     public static void menu(){
-        String menu = "[1] Deal\n" +
+        System.out.println("Deck of Cards");
+        String menu = "[1] Create cards\n" +
                 "[2] Shuffle\n" +
                 "[3] Display\n" +
-                "[4] create cards\n"+
+                "[4] Deal\n"+
                 "[5] Exit\n";
 
 
@@ -72,17 +79,18 @@ public class DeckOfCards {
         boolean end = false;
         userAction = userAction.toLowerCase();
         switch (userAction) {
-            case "1":
+            case "4":
             case "deal":
                 //call function deal
                 System.out.println("Deal");
+                System.out.print("How many?: ");
+                deal(getInput(input));
                 end = false;
                 break;
             case "2":
             case "shuffle":
                 //call function shuffle card
                 cards = shuffleCards(cards);
-                System.out.println("Cards is already shuffle!");
                 end = false;
                 break;
             case "3":
@@ -91,7 +99,7 @@ public class DeckOfCards {
                 displayCards(cards);
                 end = false;
                 break;
-            case "4":
+            case "1":
             case "create":
                 //create another deck of cards
                 createCards();
@@ -104,7 +112,7 @@ public class DeckOfCards {
                 end = true;
                 break;
             default:
-                System.out.println("WARNING: Please choose a valid action!");
+                System.out.println("\n\nWARNING: Please choose a valid action!\n\n");
                 end = false;
         }
         return end;
@@ -116,6 +124,10 @@ public class DeckOfCards {
      * @param cards
      */
     private static void displayCards(ArrayList<Card> cards){
+        if(cards.size() <= 0){
+            System.out.println("\n\nWARNING:The Deck of cards is empty!\n\n");
+            return;
+        }
         int counter = 0;
         for(int ctr = 0; ctr < cards.size(); ctr++){
             if(counter % 13 == 0)
@@ -125,7 +137,7 @@ public class DeckOfCards {
             System.out.print(cards.get(ctr).getCard()+" ");
             counter++;
         }
-        System.out.println("");
+        System.out.printf("\nThe number of cards: %d\n",cards.size());
     }
 
     /**
@@ -136,9 +148,30 @@ public class DeckOfCards {
 
     private static ArrayList<Card> shuffleCards(ArrayList<Card> cards )
     {
+        if(cards.size() <= 0){
+            System.out.println("\n\nWARNING: The Deck of Card is Empty!\n\n");
+            return cards;
+        }
         Collections.shuffle(cards);
+        System.out.println("\n\nCards is already shuffled!\n\n");
         return cards;
+
     }
 
+    /**
+     * This function is responsible for dealing the cards
+     * @params dealVal cards
+     */
 
+    private static void deal(int dealVal){
+        if(dealVal > cards.size()) {
+            System.out.println("\n\nWARNING: The amount of deal should not be \ngreater than the number of cards");
+            return;
+        }
+        for(int ctr = 0; ctr < 52; ctr++){
+            System.out.print(cards.get(ctr).getCard()+" ");
+            cards.remove(cards.get(ctr));
+        }
+        System.out.println("\n");
+    }
 }
--- DeckOfCards/src/Main.java
+++ DeckOfCards/src/Main.java
@@ -5,7 +5,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         DeckOfCards deckOfCards = new DeckOfCards();
-        deckOfCards.createCards();
         while(true) {
             deckOfCards.menu();
             if(deckOfCards.processAction(in.nextLine())){
